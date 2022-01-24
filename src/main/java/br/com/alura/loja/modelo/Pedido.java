89aee6898a21c1812607b57d6a5b748e1337f5c9
ViniciosG <viniciossantanag@gmail.com>
2022-01-24 10:02:42 -0300

    Relacionamentos e Mapeamentos

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,89 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="valor_total")
+	private BigDecimal valorTotal;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL) //tudo que fizer no Pedido, faz no item Pedido
+	private List<ItemPedido> itens = new ArrayList<>(); //inicia uma lista não nula para evitar ficar fazendo ifs.
+	
+	public Pedido() {
+	}
+	
+	
+	public Pedido(Cliente cliente) {
+		super();
+		this.cliente = cliente;
+	}
+	
+	public void adicionarItem(ItemPedido item) {
+		item.setPedido(this);
+		this.itens.add(item);
+	}
+
+
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+
+	
+
+}
